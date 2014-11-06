dff578c5854e17490f550ae41d791c9972ee1639
Keith-Cavanaugh <kcavanaugh3@gatech.edu>
2014-11-06 18:10:36 -0500

    Completed Splash + Game Scene
    Splash screen is now set up to move to the main game scene, where we
    should start plopping all of our code

---
+++ Assets/Scripts/SplashControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashControl : MonoBehaviour {
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp() {
+		Application.LoadLevel (1);
+		}
+}

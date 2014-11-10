a0474b484e538cfe7d2d529d4f605fc86bc0204a
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-11-09 20:44:36 -0500

    basic astroid tunnel

--- Assets/Scripts/GenerateEnvironment.cs
+++ Assets/Scripts/GenerateEnvironment.cs
@@ -6,15 +6,23 @@ public class GenerateEnvironment : MonoBehaviour {
 	public GameObject astPrefab;
 	// Use this for initialization
 	void Start () {
-		makeAstroidLineForLeo ();
+		makeAstroidTunnel ();
 	}
 
-	void makeAstroidLineForLeo(){
+	void makeAstroidTunnel(){
 		for (int z = 0; z<10000; z+=200) {
-			GameObject a = Instantiate (astPrefab, new Vector3(20f,5f,z*1f), Quaternion.identity) as GameObject;
-			GameObject b = Instantiate (astPrefab, new Vector3(-40f,5f,z*1f), Quaternion.identity) as GameObject;
-			a.transform.localScale = new Vector3(10f,10f,10f);
-			b.transform.localScale = new Vector3(20f,20f,20f);
+			makeAstroidRing(0f, 0f, z, 300f, 30);
+		}
+	}
+
+	void makeAstroidRing(float x, float y, float z, float radius, int num){
+		Vector3 center = new Vector3 (x, y, z);
+		for (float theta = 0f; theta<2*Mathf.PI; theta+= Mathf.PI*2/num) {
+			Vector3 myCenter = center;
+			myCenter.x+= Mathf.Sin(theta)*radius;
+			myCenter.y+= Mathf.Cos(theta)*radius;
+			GameObject a = Instantiate (astPrefab, myCenter, Quaternion.identity) as GameObject;
+			a.transform.localScale = new Vector3(30f,30f,30f);
 		}
 	}
 	

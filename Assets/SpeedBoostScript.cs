ca2bd37c4be445268457769dd6b03bfb1cc27d08
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-11-30 19:29:53 -0500

    add boost ring prefab

---
+++ Assets/SpeedBoostScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoostScript : MonoBehaviour {
+	private AvatarScript avatarS;
+	
+	void Start ()
+	{
+		avatarS = GameObject.Find ("Avatar").GetComponent<AvatarScript> ();
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			avatarS.addFreeBoostTime(2f);
+			Destroy (gameObject);
+		}
+
+	}
+}

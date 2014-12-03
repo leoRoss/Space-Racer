2dd9ac606b6ce40b8c65741662920e099d922bd5
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-12-03 00:52:56 -0500

    run into pick ups but loose health

---
+++ Assets/HContactD.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HContactD : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	private GameLogic gameLogicScript;
+	private AvatarScript aScript;
+	
+	void Start ()
+	{
+		gameLogicScript = GameObject.Find ("GameLogic").GetComponent<GameLogic> ();
+		aScript = GameObject.Find ("Avatar").GetComponent<AvatarScript> ();
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		
+		Start ();
+		if (other == null)
+			return;
+		
+		if (other.tag == "Boundary" || other.tag == "Enemy")
+		{
+			return;
+		}
+		
+		if (explosion != null)
+		{
+			Instantiate(explosion, transform.position, transform.rotation);
+		}
+		
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameLogicScript.AvatarCollidedWithWeakAstroid();
+			gameLogicScript.AddHealth();
+		}
+		
+		Destroy (gameObject);
+	}
+}
